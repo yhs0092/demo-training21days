f4282d42381a5c7bf4f7944155c1c69d242a8e76
yaohaishi <yaohaishi@huawei.com>
2019-02-13 10:40:27 +0800

    consumer: add RestTemplate invocation example

--- helloworld-consumer/src/main/java/microservice/demo/training21days/consumer/service/HelloConsumerService.java
+++ helloworld-consumer/src/main/java/microservice/demo/training21days/consumer/service/HelloConsumerService.java
@@ -6,6 +6,9 @@ import javax.ws.rs.QueryParam;
 
 import org.apache.servicecomb.provider.pojo.RpcReference;
 import org.apache.servicecomb.provider.rest.common.RestSchema;
+import org.apache.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import microservice.demo.training21days.provider.service.HelloService;
 
@@ -16,10 +19,23 @@ public class HelloConsumerService {
   @RpcReference(microserviceName = "provider", schemaId = "hello")
   private HelloService helloService;
 
+  // RestTemplate调用方式需要创建一个 ServiceComb 的 RestTemplate
+  private RestTemplate restTemplate = RestTemplateBuilder.create();
+
   @Path("/hello")
   @GET
   public String sayHello(@QueryParam("name") String name) {
     // RPC 调用方式体验与本地调用相同
     return helloService.sayHello(name);
+  }
+
+  @Path("/helloRT")
+  @GET
+  public String sayHelloRestTemplate(@QueryParam("name") String name) {
+    // RestTemplate 使用方式与原生的Spring RestTemplate相同，可以直接参考原生Spring的资料
+    // 注意URL不是 http://{IP}:{port} ， 而是 cse://{provider端服务名} ， 其他部分如path/query等与原生调用方式一致
+    ResponseEntity<String> responseEntity =
+        restTemplate.getForEntity("cse://provider/provider/v0/hello/" + name, String.class);
+    return responseEntity.getBody();
   }
 }

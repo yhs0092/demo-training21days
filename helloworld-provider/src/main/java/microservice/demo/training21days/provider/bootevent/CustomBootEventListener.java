f4358b1152df1264a4a1b0e79f658a6e3d804595
yaohaishi <yaohaishi@huawei.com>
2019-02-15 20:32:23 +0800

    provider: custom BootListener watch AFTER_REGISTRY and BEFORE_CLOSE

--- helloworld-provider/src/main/java/microservice/demo/training21days/provider/bootevent/CustomBootEventListener.java
+++ helloworld-provider/src/main/java/microservice/demo/training21days/provider/bootevent/CustomBootEventListener.java
@@ -11,12 +11,19 @@ public class CustomBootEventListener implements BootListener {
   private static final Logger LOGGER = LoggerFactory.getLogger(CustomBootEventListener.class);
 
   public void onBootEvent(BootEvent bootEvent) {
-    if (!EventType.AFTER_REGISTRY.equals(bootEvent.getEventType())) {
-      return;
+    // BootEvent中的EventType有多种
+    switch (bootEvent.getEventType()) {
+      case AFTER_REGISTRY: // 微服务实例注册成功
+        LOGGER.info("=============================");
+        LOGGER.info("Service startup completed!");
+        LOGGER.info("=============================");
+        break;
+      case BEFORE_CLOSE:   // 微服务进程即将退出
+        LOGGER.info("=============================");
+        LOGGER.info("JVM process is closing!");
+        LOGGER.info("=============================");
+        break;
+      default:
     }
-
-    LOGGER.info("=============================");
-    LOGGER.info("Service startup completed!");
-    LOGGER.info("=============================");
   }
 }

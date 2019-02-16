b755699c5dc4ca9b13733578a11156e43649c2e7
yaohaishi <yaohaishi@huawei.com>
2019-02-16 19:16:33 +0800

    provider: modify REST interface

--- helloworld-provider/src/main/java/microservice/demo/training21days/provider/service/HelloService.java
+++ helloworld-provider/src/main/java/microservice/demo/training21days/provider/service/HelloService.java
@@ -3,17 +3,17 @@ package microservice.demo.training21days.provider.service;
 import java.util.Date;
 
 import org.apache.servicecomb.provider.rest.common.RestSchema;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestSchema(schemaId = "hello")        // 该注解声明这是一个REST接口类，CSEJavaSDK会扫描到这个类，根据它的代码生成接口契约
 @RequestMapping(path = "/provider/v0") // @RequestMapping是Spring的注解，这里在使用Spring MVC风格开发REST接口
 public class HelloService {
-  @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET)
-  public String sayHello(@PathVariable(value = "name") String name) {
+  @RequestMapping(path = "/hello", method = RequestMethod.GET)
+  public String sayHello(@RequestParam(value = "name") String name) {
     return "Hello," + name;
   }
 

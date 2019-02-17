1d363fbb8bc8873285618a6188aede7b1cf81689
yaohaishi <yaohaishi@huawei.com>
2019-02-17 18:38:16 +0800

    provider: add dynamic config callback

--- helloworld-provider/src/main/java/microservice/demo/training21days/provider/service/HelloService.java
+++ helloworld-provider/src/main/java/microservice/demo/training21days/provider/service/HelloService.java
@@ -3,6 +3,8 @@ package microservice.demo.training21days.provider.service;
 import java.util.Date;
 
 import org.apache.servicecomb.provider.rest.common.RestSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,15 @@ import com.netflix.config.DynamicStringProperty;
 @RequestMapping(path = "/provider/v0") // @RequestMapping是Spring的注解，这里在使用Spring MVC风格开发REST接口
 public class HelloService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(HelloService.class);
+
   private DynamicStringProperty sayHelloPrefix = DynamicPropertyFactory
-      .getInstance().getStringProperty("hello.sayHelloPrefix", "");
+      .getInstance().getStringProperty("hello.sayHelloPrefix", ""
+          , notifyConfigRefreshed());
+
+  private Runnable notifyConfigRefreshed() {
+    return () -> LOGGER.info("config[hello.sayHelloPrefix] changed to [{}]!", sayHelloPrefix.getValue());
+  }
 
   @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET)
   public String sayHello(@PathVariable(value = "name") String name) {

781ff648d9f676a0d43f85954811387257bb47eb
yaohaishi <yaohaishi@huawei.com>
2019-02-17 16:08:13 +0800

    provider: get hello prefix from config

--- helloworld-provider/src/main/java/microservice/demo/training21days/provider/service/HelloService.java
+++ helloworld-provider/src/main/java/microservice/demo/training21days/provider/service/HelloService.java
@@ -9,12 +9,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.config.DynamicStringProperty;
+
 @RestSchema(schemaId = "hello")        // 该注解声明这是一个REST接口类，CSEJavaSDK会扫描到这个类，根据它的代码生成接口契约
 @RequestMapping(path = "/provider/v0") // @RequestMapping是Spring的注解，这里在使用Spring MVC风格开发REST接口
 public class HelloService {
+
+  private DynamicStringProperty sayHelloPrefix = DynamicPropertyFactory
+      .getInstance().getStringProperty("hello.sayHelloPrefix", "");
+
   @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET)
   public String sayHello(@PathVariable(value = "name") String name) {
-    return "Hello," + name;
+    return sayHelloPrefix.getValue() + name;
   }
 
   @PostMapping(path = "/greeting")
